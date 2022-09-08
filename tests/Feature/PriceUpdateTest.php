eabae7c992455bbd580e406f1f8c40ec0b63f2f7
Putra Prima Arhandi <putraprima@gmail.com>
2022-09-09 05:23:26 +0700

    Add setup fuction biar clean test nya

--- tests/Feature/PriceUpdateTest.php
+++ tests/Feature/PriceUpdateTest.php
@@ -10,6 +10,17 @@ use Tests\TestCase;
 class PriceUpdateTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->price = Price::create([
+            'buy' => 900000,
+            'sell' => 930000,
+            'date' => date('Y-m-d'),
+        ]);
+    }
+
     /**
      * A basic feature test example.
      *
@@ -19,12 +30,7 @@ class PriceUpdateTest extends TestCase
     public function test_buy_is_required()
     {
         //buka halaman /price/create
-        $price = Price::create([
-            'buy' => 900000,
-            'sell' => 930000,
-            'date' => date('Y-m-d'),
-        ]);
-        $response = $this->patch('/price/' . $price->id, [
+        $response = $this->patch('/price/' . $this->price->id, [
             'sell' => '900000',
             'buy' => '',
             'date' => date('Y-m-d'),
@@ -39,14 +45,7 @@ class PriceUpdateTest extends TestCase
     public function test_sell_is_required()
     {
         //buka halaman /price/create
-
-        $price = Price::create([
-            'buy' => 900000,
-            'sell' => 930000,
-            'date' => date('Y-m-d'),
-        ]);
-
-        $response = $this->patch('/price/' . $price->id, [
+        $response = $this->patch('/price/' . $this->price->id, [
             'sell' => '',
             'buy' => '900000',
             'date' => date('Y-m-d'),

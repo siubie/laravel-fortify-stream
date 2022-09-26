53bf19d6045b345021d406f89a1cc8af7d0050e7
ppa <putraprima@gmail.com>
2022-09-26 09:53:03 +0700

    login test

--- cypress/e2e/invest-app/login.cy.js
+++ cypress/e2e/invest-app/login.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe("User Can Open Login Page", () => {
     it("Login Page Can Be Open and have correct specification", () => {
-        cy.visit("http://localhost:8000/");
+        cy.visit("http://inves.test");
         cy.get("h4").should("have.text", "Login");
         cy.get(":nth-child(2) > label").should("have.text", "E-mail");
         cy.get(".control-label").should("have.text", "Password");
@@ -9,13 +9,13 @@ describe("User Can Open Login Page", () => {
     });
 
     it("User Can Login", () => {
-        cy.visit("http://localhost:8000/");
+        cy.visit("http://inves.test");
         cy.get("h4").should("have.text", "Login");
         cy.get(":nth-child(2) > label").should("have.text", "E-mail");
         cy.get(".control-label").should("have.text", "Password");
         cy.get(".btn").contains("Login").and("be.enabled");
 
-        cy.get(":nth-child(2) > .form-control").type("zkoss@example.org");
+        cy.get(":nth-child(2) > .form-control").type("eli14@example.com");
         cy.get(":nth-child(3) > .form-control").type("password");
         cy.get(".btn").click();
         cy.get("h1").contains("Blank Page");
@@ -24,7 +24,7 @@ describe("User Can Open Login Page", () => {
     });
 
     it("User Cannot Login", () => {
-        cy.visit("http://localhost:8000/");
+        cy.visit("http://inves.test");
         cy.get("h4").should("have.text", "Login");
         cy.get(":nth-child(2) > label").should("have.text", "E-mail");
         cy.get(".control-label").should("have.text", "Password");
